2c37c7629e0af181afe898a44e3995f87b2f5208
jiolivar <javier.olivares.13@sansano.usm.cl>
2021-11-26 00:34:56 -0300

    fulcrum sendinformeichon

--- servidores/Fulcrum.go
+++ servidores/Fulcrum.go
@@ -7,12 +7,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 
 	//"net"
 	"os"
-	"strings"
 
 	pb "github.com/fabiusinfo/StarWars/proto"
+	"google.golang.org/grpc"
 	//	"google.golang.org/grpc"
 )
 
@@ -54,83 +55,16 @@ func crearArchivo(path string) {
 
 //var delet int = 1
 
-/*
-func (s *server) SendPlays(ctx context.Context, in *pb.SendRequest) (*pb.SendReply, error) {
 
-//aqui implementar la escritura del archivo de texto
-	//commando := GetMessage()
-	comando := "Latierra4 Valpo 8"
-	palabra := strings.Split(commando, " ")
-	planeta := palabra[0]
-	var path = "RP/" + planeta + ".txt"
+func (s *server) SendInformationF(ctx context.Context, in *pb.SendRequest) (*pb.SendReply, error) {
 
-		if delet == 1 {
-		delet = 0
-			nombreArchivo = path // El nombre o ruta absoluta del archivo
-			err := osRemove(nombreArchivo)
-			if err != nil {
-				fmt.Printf("Error eliminando achivo: %v\n", err)
-			} else {
-				fmt.Println(" ")
-			}
-		}
-
-	crarArchivo(path)
-
-	// añadir al texto
-	b, errtxt := ioutil.ReadFile(path)
-
-	if errtxt != nil {
-	log.Fatal(errtxt)
-	}
-
-	nmbre_ciudad := palabra[1]
-	cantidad_soldados_rebeldes : palabra[2]
-
-	b = append(b, []bye( nombre_planea + " " +nombre_ciudad +" " + cantidad_soldados_rebeldes +" \n")...)
-	errtxt = ioutil.WrteFile(path, b, 0644)
-
-	if errtxt != nil {
-		log.Fatal(errtxt)
-		}
-
-	fm.Println("Se recibe... Player: " + in.GetPlayer() + " / Play:  " + in.GetPlay() + " / Stage: " + in.GetStage())
-	return &pb.SndReply{Stage: "Amongus", Alive: true}, nil
-}
-*/
-func main() {
-	//nos convertios en servidor
-	/*
-	X := "none"
-	go func() {
-		listner, err := net.Listen("tcp", ":9000")
-
-		if err != nil {
-			panic("cannot connect with server " + err.Error())
-		}
-
-		serv := grpc.NewServer()
-		pb.RegisterStarWarsServiceServer(serv, &server{})
-		if err = serv.Serve(listner); err != nil {
-			panic("cannot initialize the server" + err.Error())
-
-		}
-
-	}()
-	
-	fmt.Println("Esperando un: oye!")
-	fmt.Scanln(&X)
-*/
 	//aqui implementar la escritura del archivo de texto
-
-	
-
-	comando := "Latierra4 Valpo 8"
-	palabra := strings.Split(comando, " ")
-	nombre_planeta := palabra[0]
-	nombre_ciudad := palabra[1]
-	cantidad_soldados_rebeldes := palabra[2]
+	commando := in.GetCommand()
+	nombre_planeta := in.GetPlanet()
+	nombre_ciudad := in.GetCity()
+	cantidad_soldados_rebeldes := in.GetValue()
 	var path = "servidores/RP/"+nombre_planeta + ".txt"
+	fmt.Println(commando)
 	fmt.Println(nombre_planeta)
 	fmt.Println(nombre_ciudad)
 	fmt.Println(cantidad_soldados_rebeldes)
@@ -151,4 +85,62 @@ func main() {
 		log.Fatal(errtxt)
 	}
 
+
+	return &pb.SendReply{Ip: "a", Port: "9000"}, nil
+}
+
+
+func main() {
+	//nos convertios en servidor
+	
+	X := "none"
+	go func() {
+		listner, err := net.Listen("tcp", ":9000")
+
+		if err != nil {
+			panic("cannot connect with server " + err.Error())
+		}
+
+		serv := grpc.NewServer()
+		pb.RegisterStarWarsServiceServer(serv, &server{})
+		if err = serv.Serve(listner); err != nil {
+			panic("cannot initialize the server" + err.Error())
+
+		}
+
+	}()
+	
+	fmt.Println("Esperando un: oye!")
+	fmt.Scanln(&X)
+
+	//aqui implementar la escritura del archivo de texto
+
+	
+/*
+	comando := "Latierra4 Valpo 8"
+	palabra := strings.Split(comando, " ")
+	nombre_planeta := in.GetPlanet()
+	nombre_ciudad := in.GetCity()
+	cantidad_soldados_rebeldes := in.GetValue()
+	var path = "servidores/RP/"+nombre_planeta + ".txt"
+	fmt.Println(nombre_planeta)
+	fmt.Println(nombre_ciudad)
+	fmt.Println(cantidad_soldados_rebeldes)
+	
+	crearArchivo(path)
+
+	// añadir al texto
+	b, errtxt := ioutil.ReadFile(path)
+
+	if errtxt != nil {
+		log.Fatal(errtxt)
+	}
+
+	b = append(b, []byte(nombre_ciudad+" "+cantidad_soldados_rebeldes+" \n")...)
+	errtxt = ioutil.WriteFile(path, b, 0644)
+
+	if errtxt != nil {
+		log.Fatal(errtxt)
+	}
+*/
 }
