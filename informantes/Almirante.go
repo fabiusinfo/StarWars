6bf4b6c2639e8377572bf3e2825ec1d11ed35621
Fabio <fabian.arancibiab@sansano.usm.cl>
2021-11-29 18:30:29 -0300

    bla

--- informantes/Almirante.go
+++ informantes/Almirante.go
@@ -127,5 +127,6 @@ func main() {
 		log.Printf("Greeting: %s", r2.GetClock())
 
 		ReadYourWrites = append(ReadYourWrites, WriteStruct{message[0], message[1], message[2], message[3], r2.GetClock(), r.GetIp()})
+		fmt.Fprintln("lo que se guardó en ReadYourWrites: " + message[0] + " " + message[1] + " " + message[2] + " " + message[3] + " " + r2.GetClock() + " " + r.GetIp())
 	}
 }
